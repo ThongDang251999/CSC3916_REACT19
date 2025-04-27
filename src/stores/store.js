4f2ed3c91fd094b30a63600a09c526aa8010a119
ThongDang251999 <minhthong251999@gmail.com>
2025-04-27 16:59:23 -0600

    updated

--- src/stores/store.js
+++ src/stores/store.js
@@ -2,11 +2,11 @@ import { configureStore } from '@reduxjs/toolkit';
 import { thunk } from 'redux-thunk';
 import authReducer from "../reducers/authReducer";
 import movieReducer from "../reducers/movieReducer";
+
 const middlewares = [thunk];
 
 if (process.env.NODE_ENV === 'development') {
     const { logger } = require('redux-logger');
-
     middlewares.push(logger);
 }
 
@@ -16,7 +16,7 @@ const store = configureStore({
         movie: movieReducer
     },
     middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware().concat(thunk)
+        getDefaultMiddleware().concat(middlewares)
 });
 
 export default store;
